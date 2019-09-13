400fa223b3952b376a99af116e266e98b57d0e4d
shanisingh961 <55283159+shanisingh961@users.noreply.github.com>
2019-09-13 18:35:43 +0530

    Add files via upload

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  onNotify(){
+    alert("hello");
+  }
+  share(price) {
+    window.alert(price);
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
